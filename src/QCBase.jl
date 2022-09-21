011f5def1321eac0d152c2983510fbe701b48e66
Nick Mayhall <nmayhall@vt.edu>
2022-09-20 20:30:40 -0400

    defined nothing functions

--- src/QCBase.jl
+++ src/QCBase.jl
@@ -13,8 +13,12 @@ export Atom
 export Molecule
 export MOCluster
 
+n_orb() = nothing
+dim_tot() = nothing
+write_xyz() = nothing
+
 export n_orb
 export dim_tot
-export write_xyz 
+export write_xyz
 
 end
